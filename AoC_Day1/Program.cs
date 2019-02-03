b2ac22e729cc0c95cfa8e6cbefe5ea10a497492c
Adam Bailey <nomadthegrey@yahoo.com>
2019-02-03 01:11:27 -0500

    AoC_Day1_Part1

--- AoC_Day1/Program.cs
+++ AoC_Day1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,13 @@ namespace AoC_Day1
 {
     class Program
     {
+        static List<int> FrequencyList = new List<int>();
         static void Main(string[] args)
         {
+            int sum = File.ReadAllLines(@"C:\Projects\Advent_of_Code\Day_1\AoC_Day1\PuzzleInput.txt").Sum(x=>Convert.ToInt32(x));
+            
+            Console.WriteLine("Total: {0}",sum);
+            Console.ReadKey();
         }
     }
 }
